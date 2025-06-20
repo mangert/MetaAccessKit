de9a192b1017ea3f6b84bbbab185042de5670ef1
mangert <mangert@yandex.ru>
2025-06-20 23:39:38 +0300

    project settings updated

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,39 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "hardhat-gas-reporter";
+import dotenv from "dotenv";
+
+dotenv.config(); 
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.28",
+  solidity: {
+    version: "0.8.29",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 1000,
+      },
+    },
+  },
+  networks: {
+    hardhat: {
+      chainId: 1337,
+      initialBaseFeePerGas: 0,
+    },
+    sepolia: {
+      url: process.env.ALCHEMY_API_URL || "",
+      accounts: process.env.PRIVATE_KEY ? [`0x${process.env.PRIVATE_KEY}`] : [],
+      chainId: 11155111,
+    },
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY || "",
+  },
+  gasReporter: {
+    //enabled: process.env.REPORT_GAS === "true",
+    enabled: false,
+    currency: "ETH", 
+  },
 };
 
 export default config;
