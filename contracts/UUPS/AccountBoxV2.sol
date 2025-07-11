aecedb61af860cd66c327ec046931cfd026041d4
mangert <mangert@yandex.ru>
2025-07-12 01:20:18 +0300

    tests upgradable added

---
+++ contracts/UUPS/AccountBoxV2.sol
@@ -0,0 +1,92 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.29;
+
+import { ETHAccountV2 } from "../factory/ETHAccountV2.sol";
+import "@openzeppelin/contracts/proxy/Clones.sol";
+import "../libs/IDGenerator.sol";
+import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+
+/**
+ * @title AccountBoxV2  
+ * @notice Контракт-фабрика в обновляемой редакции (паттерн UUPSUpgradeable)
+ * вторая редакция реализации - добавлен общий счетчик созданных аккаунтов и функция-геттер
+ */
+contract AccountBoxV2 is Initializable, UUPSUpgradeable, OwnableUpgradeable{
+    
+    address public implementation; //ссылка на контракт-шаблон
+    
+    //хранилище созданных счетов
+    mapping (address client => mapping (bytes4 id => address account)) private accounts;    
+    //счетчики для адресов
+    mapping(address => uint8) public userCounters;    
+
+    /**
+     * @notice событие сообщает о создании нового счета
+     * @param owner - адрес владелеца
+     * @param id - идентификатор 
+     * @param account - адрес созданного счета
+     */
+    event AccountCreated(address indexed owner, bytes4 indexed id, address account);    
+    
+    /**
+     * @notice - функция инициализации - вместо конструктора
+     * @param trustedForwarder -
+     * @param initialOwner -
+     */
+    function initialize(address trustedForwarder, address initialOwner) public initializer {
+        implementation = address(new ETHAccountV2(trustedForwarder));
+        __Ownable_init(initialOwner);
+        __UUPSUpgradeable_init();
+    }      
+    
+    /**
+     * @notice функция создает новый минимальный прокси для шаблонного контракта-счета
+     */
+    function createClone() external returns (address) {
+        
+        address payable clone = payable (Clones.clone(implementation)); //создаем клон
+
+        ETHAccountV2(clone).initialize(userCounters[msg.sender]++, msg.sender); //инициализируем и увеличиваем для пользователя счетчик
+        
+        bytes4 accountId = ETHAccountV2(clone).accountID(); //получаем ID
+        accounts[msg.sender][accountId] = clone; //записываем в хранилище        
+        
+        emit AccountCreated(msg.sender, accountId, clone);
+        
+        return clone;
+    }
+
+    /**
+     * @notice функция возращает адрес прокси-аккаунта исходя из адреса владельца и его индекса создания
+     * @param owner - адрес владельца для поиска
+     * @param index - индекс (по порядку создания)
+     */
+    function getAccountByIndex(address owner, uint8 index) public view returns (address) {
+        bytes4 id = IDGenerator.computeId(owner, index);
+        return accounts[owner][id];
+    }      
+
+    /**
+     * @notice функция возращает адрес прокси-аккаунта исходя из адреса владельца и его id
+     * добавлена во второй реализации для демонстрации "обновляемости"
+     * @param owner - адрес владельца для поиска
+     * @param _id - индекс (по порядку создания)
+     */
+    function getAccountById(address owner, bytes4 _id) public view returns (address) {
+        
+        return accounts[owner][_id];
+    }
+
+    /**
+     * @notice функция проверяет права доступа в UUPS. Вызывается внутри _upgrade-функций
+     * в текущей реализации вся логика вынесена в модификатор onlyOwner
+     * @param newImplementation - ссылка на обновленную реализацию
+     */
+    function _authorizeUpgrade(address newImplementation)
+        internal
+        override
+        onlyOwner
+    {}
+}
--- test/UUPS/uups.test.ts
+++ test/UUPS/uups.test.ts
@@ -1,5 +1,5 @@
 import { loadFixture, ethers, expect  } from "../setup";
-import { IETHAccount, AccountBoxV1 } from "../../typechain-types";
+import { IETHAccount, AccountBoxV1, AccountBoxV2 } from "../../typechain-types";
 import { signMetaTx } from "../metaERC2771/helpers"; 
 import { upgrades } from "hardhat";
 
@@ -28,42 +28,40 @@ describe("UUPS-upgradable tests", function() {
 
     describe("deployment tеsts", function() { //примитивный тест на деплой - просто проверить, что общая часть работает
         it("should be deployed", async function() {
-            const { user0, user1, forwarder, proxyBox} = await loadFixture(deploy); 
-            
-            console.log('UUPS proxy addr: ', await proxyBox.getAddress());
-            const implAddress = await upgrades.erc1967.getImplementationAddress(await proxyBox.getAddress());
-            console.log("Implementation:", implAddress);
+            const { user0, user1, forwarder, proxyBox} = await loadFixture(deploy);            
 
-       
+            const implAddr = await proxyBox.implementation(); //получаем адрес задеплоенный шаблон контракта-счета            
+            const impl = await ethers.getContractAt("ETHAccountV2", implAddr); //сделаем из него контракт
+
+            const forwarderAddress = await impl.trustedForwarder(); //посмотрим, с каким форвардером нам фабрика записала шаблон           
+            expect(forwarderAddress).to.equal(forwarder.target);  //и убедимся, что с верным      
 
         });
     });
 
-    /*describe("clone tests", function() { //тесты создания клонов
-        it("should create clones", async function() {//тест проверяет, что клон создается, умеет выполнять транзакции и записывается в хранилище
+    describe("upgrade tests", function() { //тесты на обновляемость
+        it("should create clones before upgrade factory ", async function() {//тест проверяет, что через обновляемую фабирку клон создается, умеет выполнять транзакции и записывается в хранилище
             
-            const { user0, user1, accountBox} = await loadFixture(deploy); 
+            const { user0, user1, proxyBox} = await loadFixture(deploy); 
 
+            //для начала запустим создание аккаунта на первой версии фабрики
             //сначала сделаем симуляцию и получим адрес контракта-клона для юзера0
-            const predictedClone0 = accountBox.getFunction("createClone");
+            const predictedClone0 = proxyBox.getFunction("createClone");
             const clone0Addr0 = (await predictedClone0.staticCallResult())[0]; //получаем возвращаемое значение - адрес нового акканута            
             
             //а теперь запустим транзакцию
-            const tx0 = await accountBox.createClone();
+            const tx0 = await proxyBox.createClone();
             await tx0.wait(1);
             
             //получим адрес созданного клона из хранилища accountBox
-            const account0Address = await accountBox.getAccountByIndex(user0, 0);
-
-            //проверим на всякий случай, что адреса совпали
-            expect(clone0Addr0).eq(account0Address);            
+            const account0Address = await proxyBox.getAccountByIndex(user0, 0);
             
             //теперь получим контракт-аккаунт из адреса
             const account0 = await ethers.getContractAt("ETHAccountV2", account0Address);
             const accountID = await account0.accountID(); //и узнаем id аккаунта
             
             //и проверим, что наша транзакция правильное событие эмитировала
-            await expect(tx0).to.emit(accountBox, "AccountCreated").withArgs(user0, accountID, account0Address);
+            await expect(tx0).to.emit(proxyBox, "AccountCreated").withArgs(user0, accountID, account0Address);
             
             //проверим, сможем ли мы работать с нашим вновь созданным аккаунтом
             //сначала закинем деньги
@@ -76,39 +74,66 @@ describe("UUPS-upgradable tests", function() {
             txWithdraw.wait(1);
             await expect(txWithdraw).changeEtherBalances([account0Address, user0], [-50n, 50n]);            
         });
-
-        it("should create several clones", async function() { //тест проверяет, что мы можем несколько клонов создавать для разных юзеров
+        
+        it("should upgrade accountBox", async function() { //тест проверяет, что мы можем обновить фабрику и она нормально сохранит данные, сохранившиеся с первой версии
             
-            const { user0, user1, user2, accountBox, forwarder} = await loadFixture(deploy); 
-
-            const users = [user0, user1, user2]; //запихнула в массив, чтобы циклы можно было крутить            
+            const { user1, proxyBox} = await loadFixture(deploy); 
+            //запомним адрес, с которым у нас задеплоилась сама имплементация фабрики
+            const implAddressBeforeUp = await upgrades.erc1967.getImplementationAddress(await proxyBox.getAddress());           
+                       
+            //создадим для юзера1 счет и забросим на него деньги
+            const tx1 = await proxyBox.connect(user1).createClone();
+            await tx1.wait(1);                 
             
-            //создадим для каждого юзера по 2 счета
-            for(let i=0; i!=3; ++i) {                
-                
-                const tx1 = await accountBox.connect(users[i]).createClone();
-                await tx1.wait(1);                 
-                
-                const tx2 = await accountBox.connect(users[2 - i]).createClone();
-                await tx2.wait(1);                                 
-            }
-            //проверим для каждого юзера создались 2 аккаунта, с нужным trustedForwarder и непустым ID
-            for(let i=0; i != 3; ++i) {                                
-                
-                const counter = await accountBox.userCounters(users[i]); //сначала посмотрим счетчик
-                expect(counter).eq(2); //и проверим, что их 2
-                
-                for(let j = 0; j != 2; ++j) { //теперь для каждого счета
-                    //получим контракт кошелька
-                    const address = await accountBox.getAccountByIndex(users[i], j);
-                    const account = (await ethers.getContractAt("ETHAccountV2", address));                                
-                    //и проверим...
-                    expect(await account.trustedForwarder()).eq(await forwarder.target); //что форвардер тот, с которым деплоили фабрику
-                    expect(await account.accountID()).not.eq(""); //и идентификатор не пустой (какой - не знаем, но это не суть важно)
-                }                
-            }          
+            const accountAddr0 = await proxyBox.getAccountByIndex(user1, 0);
+            const account0 = await ethers.getContractAt("ETHAccountV2", accountAddr0);
+            const accountID = await account0.accountID(); //получим ID, чтобы было, с чем сравнить
+                        
+            const amount = 1000n;
+            const dep = await account0.deposit({value: amount});            
+            
+            //А теперь обновим фабрику
+            const BoxV2 = await ethers.getContractFactory("AccountBoxV2");
+            const proxyBoxV2 = await upgrades.upgradeProxy(proxyBox, BoxV2);
+            //новый адрес реализации
+            const implAddressAfterUp = await upgrades.erc1967.getImplementationAddress(await proxyBox.getAddress());           
+
+            //а теперь попоробуем получить из новой фабрики адрес аккаунта, сделанный на первой версии
+            const account0AddrV2 = await proxyBoxV2.getAccountById(user1, accountID); //вызываем функцию, которой не было в версии 1      
+            //и проверим его баланс
+            const balance = await ethers.provider.getBalance(account0AddrV2);  
+            expect(balance).eq(amount);
+            
+            //проверим, что у нас с адресами 
+            expect(implAddressAfterUp).not.eq(implAddressBeforeUp); //адреса реализаций не должны совпадать
+            expect(proxyBoxV2).eq(proxyBox); //а адрес прокси наоборот, не должен измениться            
+                     
         });
 
+        it("should revert unauthorized upgrade", async function() { //тест проверяет, что мы не можем обновить фабрику от левого адреса
+            
+            const {user0, user1, proxyBox, forwarder} = await loadFixture(deploy);             
+            
+            //Пробуем обновить фабрику с постороннего аккаунта
+            const newImpl = await ethers.deployContract("AccountBoxV2"); //задеплоим реализацию
+            const newImplAddress = await newImpl.getAddress(); //возьмем адрес реализации
+
+            //соберем данные для вызова функции инициализации
+            const iface = new ethers.Interface([
+                "function initialize(address trustedForwarder, address initialOwner)"
+            ]); //сигнатура
+            const data = iface.encodeFunctionData("initialize", [
+                await forwarder.getAddress(),  // адрес твоего форвардера
+                user0.address      // правильный владелец
+            ]); //запаковываем в нужный формат
+
+            //и ожидаем, что вызов upgradeToAndCall отвалится
+            await expect(
+                proxyBox.connect(user1).upgradeToAndCall(newImplAddress, data)
+                ).to.be.revertedWithCustomError(proxyBox, "OwnableUnauthorizedAccount");
+                     
+        });
+        /*
         it("should allow meta-withdraw via forwarder", async function () { //smoke-тест для проверки работоспособности клонов с метатранзакциями
             
             const { user0, user1, forwarder, accountBox } = await loadFixture(deploy);
@@ -141,7 +166,7 @@ describe("UUPS-upgradable tests", function() {
 
             const tx = await forwarder.connect(user1).execute(fullRequest); //запускаем транзу от user1
             await expect(tx).to.changeEtherBalances([accAddr, user1.address], [-amount, amount]);
-            });
-    });*/
+            });*/
+    });
 
 });
